895d2ff750d5d7fb88529812f893714b77bb3ff4
JAEEOK <anjaeoek@nate.com>
2022-07-21 10:18:18 +0900

    firsrt

---
+++ infrainsight/src/main/java/kr/infra/com/infrainsight/InfrainsightApplication.java
@@ -0,0 +1,13 @@
+package kr.infra.com.infrainsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfrainsightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfrainsightApplication.class, args);
+	}
+
+}
---
+++ infrainsight/src/test/java/kr/infra/com/infrainsight/InfrainsightApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.infra.com.infrainsight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfrainsightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
